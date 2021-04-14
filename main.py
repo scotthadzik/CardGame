034c90e3f99aefcaa4d68fb0b8c8da5a6e395024
Scott Hadzik <scotthadzik@weber.edu>
2021-04-14 08:59:14 -0600

    Added main program

---
+++ main.py
@@ -0,0 +1,12 @@
+from Card import Card
+
+def main():
+    acornBearer = Card('Acorn Bearer', 1)
+    crystalPower = Card('Crystal Power', 1)
+
+    acornBearer.printCard()
+    crystalPower.printCard()
+
+
+if __name__ == "__main__":
+    main()
